55921746296fb1162235ab49f716f6ff4990c068
dark-217 <happyshine217@gmail.com>
2024-10-07 06:31:56 -0400

    fix: implemented outside click handler to close the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../style/menu.css";
 
 type MenuItem = {
@@ -13,6 +13,7 @@ interface MenuProps {
 
 const Menu: React.FC<MenuProps> = ({ items }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLUListElement>(null);
 
   const openMenu = () => {
     setIsOpen(true);
@@ -22,30 +23,47 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false); // Close the menu
+      }
+    };
+
+    // Add when the component is mounted
+    document.addEventListener("mousedown", handleClickOutside);
+    // Cleanup the event listener
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]); // Empty dependency array means this effect will only run once
+
   return (
     <>
       <div className="laptop-menu">
         <ul className="menu h-screen">
-          {items.map((item, index) => (
-            <>
-              {index == items.length - 1 && <hr />}
-              <li key={index} className="menu-item">
-                {item.href ? (
-                  <a
-                    href={item.href}
-                    onClick={item.onClick}
-                    className="menu-link"
-                  >
-                    {item.label}
-                  </a>
-                ) : (
-                  <button onClick={item.onClick} className="menu-button">
-                    {item.label}
-                  </button>
-                )}
-              </li>
-            </>
-          ))}
+          <div className="menu-content">
+            {items.map((item, index) => (
+              <>
+                {index == items.length - 1 && <hr />}
+                <li key={index} className="menu-item">
+                  {item.href ? (
+                    <a
+                      href={item.href}
+                      onClick={item.onClick}
+                      className="menu-link"
+                    >
+                      {item.label}
+                    </a>
+                  ) : (
+                    <button onClick={item.onClick} className="menu-button">
+                      {item.label}
+                    </button>
+                  )}
+                </li>
+              </>
+            ))}
+          </div>
         </ul>
       </div>
       <div className="mobile-menu">
@@ -55,7 +73,7 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
           </div>
         )}
         {isOpen && (
-          <ul className="menu">
+          <ul className="menu" ref={menuRef}>
             <div className="close-menu" onClick={closeMenu}>
               ✕
             </div>
